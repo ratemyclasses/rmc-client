6d35d18f9f9b63de53aa1fc0edbf4909ea4bfed2
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2021-05-17 12:06:43 -0700

    Added logout button

--- src/features/dashboard/Dashboard.jsx
+++ src/features/dashboard/Dashboard.jsx
@@ -1,3 +1,18 @@
+import { useDispatch } from "react-redux";
+import { logout } from "../auth/auth.actions";
+
 export function Dashboard() {
-  return <div>WELCOME TO THE DASHBOARD</div>;
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      WELCOME TO THE DASHBOARD
+      <button
+        className="group relative flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        onClick={() => dispatch(logout())}
+      >
+        Logout
+      </button>
+    </div>
+  );
 }
