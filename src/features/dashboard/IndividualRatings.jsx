6db0b9ed74a9bfa4263102141d3565800cba21ea
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2021-05-21 17:08:35 -0700

    Added dynamic data for course information and made createReview api work

--- src/features/dashboard/IndividualRatings.jsx
+++ src/features/dashboard/IndividualRatings.jsx
@@ -1,30 +1,50 @@
 import React from 'react';
 
-export function IndividualRatings() {
+export function IndividualRatings({ avgDifficulty, avgHoursPerWeek, wtaPercent }) {
+  const calcBarFraction = (num, max) => Math.round((num / max) * 12);
+
+  const diff = calcBarFraction(avgDifficulty, 5);
+  const hours = calcBarFraction(avgHoursPerWeek, 30);
+  const wta = Math.round(wtaPercent * 12);
+
+  let diffClass = diff !== 0 ? `${diff}/12` : 0;
+  let hoursClass = hours !== 0 ? `${hours}/12` : 0;
+  let wtaClass = wta !== 0 ? `${wta}/12` : 0;
+
+  diffClass = diff === 12 ? 'w-full' : diffClass;
+  hoursClass = hours === 12 ? 'w-full' : hoursClass;
+  wtaClass = wta === 12 ? 'w-full' : wtaClass;
+
   return (
     <div className="w-full md:w-full p-1 dark:bg-gray-800 text-gray-700 dark:text-gray-100 relative overflow-hidden">
       <a href="/" className="w-full h-full block">
         <div className="w-full">
           <div className="flex items-center justify-between text-gray-400 text-sm">
-            <p>Design</p>
-            <p>3/8</p>
+            <p>Difficulty</p>
+            <p>{avgDifficulty}</p>
           </div>
           <div className="w-full h-2 bg-green-100 rounded-full mb-4">
-            <div className="w-1/3 h-full text-center text-xs text-white bg-green-400 rounded-full" />
+            <div
+              className={`w-${diffClass} h-full text-center text-xs text-white bg-green-400 rounded-full`}
+            />
           </div>
           <div className="flex items-center justify-between text-gray-400 text-sm">
-            <p>Development</p>
-            <p>6/10</p>
+            <p>Hrs / Week</p>
+            <p>{avgHoursPerWeek}</p>
           </div>
           <div className="w-full h-2 bg-indigo-100 rounded-full mb-4">
-            <div className="w-2/3 h-full text-center text-xs text-white bg-indigo-400 rounded-full" />
+            <div
+              className={`w-${hoursClass} h-full text-center text-xs text-white bg-indigo-400 rounded-full`}
+            />
           </div>
           <div className="flex items-center justify-between text-gray-400 text-sm">
-            <p>DevOps</p>
-            <p>2/8</p>
+            <p>Would Take Again</p>
+            <p>{wtaPercent * 100}%</p>
           </div>
           <div className="w-full h-2 bg-blue-100 rounded-full mb-4">
-            <div className="w-1/4 h-full text-center text-xs text-white bg-blue-400 rounded-full" />
+            <div
+              className={`w-${wtaClass} h-full text-center text-xs text-white bg-blue-400 rounded-full`}
+            />
           </div>
         </div>
       </a>
