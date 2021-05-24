55c741810495e8f3cda7c3ab6aa561efd3985e58
Sriharsha Guduguntla <sguduguntla11@gmail.com>
2021-05-23 23:19:45 -0700

    Finished course paginator, but just need to fix alignment

---
+++ src/common/Paginator.jsx
@@ -0,0 +1,53 @@
+/* This example requires Tailwind CSS v2.0+ */
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
+import React from 'react';
+
+/* eslint-disable */
+
+export function Paginator({ offset, setOffset, curPageNum }) {
+  return (
+    <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6">
+      <div className="flex-1 flex justify-between sm:hidden">
+        <button
+          onClick={() => setOffset(offset - 7)}
+          className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+        >
+          Previous
+        </button>
+        <button
+          onClick={() => setOffset(offset + 7)}
+          className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
+        >
+          Next
+        </button>
+      </div>
+      <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+        <div>
+          <nav className="relative z-0 inline-flex rounded-md -space-x-px" aria-label="Pagination">
+            <button
+              onClick={() => setOffset(offset - 7)}
+              className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+            >
+              <span className="sr-only">Previous</span>
+              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+            {/* Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-300 text-gray-500 hover:bg-gray-50" */}
+            <button
+              aria-current="page"
+              className="z-10 bg-indigo-50 border-indigo-500 text-indigo-600 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+            >
+              {curPageNum}
+            </button>
+            <button
+              onClick={() => setOffset(offset + 7)}
+              className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+            >
+              <span className="sr-only">Next</span>
+              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/features/dashboard/CourseList.jsx
+++ src/features/dashboard/CourseList.jsx
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCourses } from '../../app/actions/course.actions';
 import { CourseListItem } from '../course/CourseListItem';
+import { Paginator } from '../../common/Paginator';
 
 export function CourseList() {
   const courses = useSelector((state) => state.course.courses);
   const college = useSelector((state) => state.college.college);
   const dispatch = useDispatch();
+  const [offset, setOffset] = useState(0);
+  const limit = 7;
 
   useEffect(() => {
     if (college) {
@@ -14,8 +17,8 @@ export function CourseList() {
         getCourses({
           collegeId: college._id,
           params: {
-            limit: 7,
-            offset: 1,
+            limit,
+            offset,
             sortBy: ['abbreviation:1', 'number:1'],
             fields: ['name', 'abbreviation', 'number']
           }
@@ -23,7 +26,7 @@ export function CourseList() {
       );
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [college, dispatch]);
+  }, [college, offset, dispatch]);
 
   if (!courses.length) {
     return <div>Loading courses...</div>;
@@ -31,9 +34,21 @@ export function CourseList() {
 
   return (
     <div className="mb-8 bg-white h-screen overflow-y-auto">
+      <div className="mt-3 ml-3">
+        <p className="text-sm text-gray-700">
+          Showing <span className="font-medium">{offset + 1}</span> to{' '}
+          <span className="font-medium">{offset + 7}</span> of{' '}
+          <span className="font-medium">15,456</span> results
+        </p>
+      </div>
       {courses.map((course) => (
         <CourseListItem key={course._id} course={course} />
       ))}
+      <Paginator
+        setOffset={setOffset}
+        offset={offset}
+        curPageNum={Math.round(offset / limit) + 1}
+      />
     </div>
   );
 }
